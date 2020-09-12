57f907d4fecae261a0b6958aa1a4cc9bf4a5c78b
mariomtz_jr <mario.martinez@comuna18.com>
2020-09-12 17:20:44 -0500

    Separated app migrations. Deleted general migrations in ecommerce app.

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.ecommerce',
+    'apps.ecommerce.category',
+    'apps.ecommerce.customer',
+    'apps.ecommerce.person',
+    'apps.ecommerce.product',
+    'apps.ecommerce.purchase',
+    'apps.ecommerce.purchaseproduct',
     'apps.ecommerce.api',
     'apps.users'
 ]
